40fdad562ca6d44fa69aba194eb351091faa0014
Truman Cheung <>
2020-09-15 18:02:46 +0800

    animated splash screen

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import 'react-native-gesture-handler';
-import React, {createRef} from 'react';
+import React, {createRef, useRef, useEffect, useState} from 'react';
+import {View, Image, Animated, StatusBar, StyleSheet} from 'react-native';
 import {enableScreens} from 'react-native-screens';
 import FlashMessage from 'react-native-flash-message';
 
@@ -8,19 +9,22 @@ import {TDContextProvider} from '~/Context/TDA';
 
 import Navigator from '~/Screen/Navigator';
 import LocaleProvider from '~/I18n';
+import Splash from '~/Component/Splash';
 
 enableScreens();
 const App = () => {
   const flashMessageRef = createRef();
   return (
-    <LocaleProvider>
-      <UserContextProvider>
-        <TDContextProvider>
-          <Navigator />
-          <FlashMessage ref={flashMessageRef} />
-        </TDContextProvider>
-      </UserContextProvider>
-    </LocaleProvider>
+    <Splash isLoaded>
+      <LocaleProvider>
+        <UserContextProvider>
+          <TDContextProvider>
+            <Navigator />
+            <FlashMessage ref={flashMessageRef} />
+          </TDContextProvider>
+        </UserContextProvider>
+      </LocaleProvider>
+    </Splash>
   );
 };
 
---
+++ src/Component/Splash/index.js
@@ -0,0 +1,82 @@
+import React, {createRef, useRef, useEffect, useState} from 'react';
+import {View, Image, Animated, StatusBar, StyleSheet} from 'react-native';
+import MaskedView from '@react-native-community/masked-view';
+import logo from '~/Asset/smile.png';
+
+const App = ({isLoaded, children}) => {
+  const [animationDone, setAnimationDone] = useState(false);
+  const loadingProgress = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    if (isLoaded) {
+      Animated.timing(loadingProgress, {
+        toValue: 100,
+        duration: 1000,
+        useNativeDriver: true,
+      }).start(() => setAnimationDone(true));
+    }
+  }, [isLoaded]);
+
+  const opacityClearToVisible = {
+    opacity: loadingProgress.interpolate({
+      inputRange: [0, 15, 30],
+      outputRange: [0, 0, 1],
+      extrapolate: 'clamp',
+      // clamp means when the input is 30-100, output should stay at 1
+    }),
+  };
+
+  const imageScale = {
+    transform: [
+      {
+        scale: loadingProgress.interpolate({
+          inputRange: [0, 10, 100],
+          outputRange: [1, 0.8, 70],
+        }),
+      },
+    ],
+  };
+
+  const appScale = {
+    transform: [
+      {
+        scale: loadingProgress.interpolate({
+          inputRange: [0, 7, 100],
+          outputRange: [1.1, 1.03, 1],
+        }),
+      },
+    ],
+  };
+
+  const fullScreenBlueLayer = animationDone ? null : (
+    <View style={[StyleSheet.absoluteFill, {backgroundColor: '#1DA1F2'}]} />
+  );
+  const fullScreenWhiteLayer = animationDone ? null : (
+    <View style={[StyleSheet.absoluteFill, {backgroundColor: '#ffffff'}]} />
+  );
+
+  return (
+    <View style={{flex: 1}}>
+      <StatusBar animated hidden={!animationDone} />
+      {fullScreenBlueLayer}
+      <MaskedView
+        style={{flex: 1}}
+        maskElement={
+          <View
+            style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Animated.Image
+              style={[{width: 200, height: 200}, imageScale]}
+              source={logo}
+            />
+          </View>
+        }>
+        {fullScreenWhiteLayer}
+        <Animated.View style={[opacityClearToVisible, appScale, {flex: 1}]}>
+          {children}
+        </Animated.View>
+      </MaskedView>
+    </View>
+  );
+};
+
+export default App;
