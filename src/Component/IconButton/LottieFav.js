3073396d882949dfb5da7a302caebcc2e55a225f
Truman Cheung <>
2020-09-23 16:52:04 +0800

    update

--- src/Component/IconButton/LottieFav.js
+++ src/Component/IconButton/LottieFav.js
@@ -1,17 +1,20 @@
 import React, {forwardRef, useEffect} from 'react';
 import {TouchableOpacity} from 'react-native';
 import LottieView from 'lottie-react-native';
-import favIcon from '~/Asset/heart.json';
+import lightFavIcon from '~/Asset/heart-light.json';
+import darkFavIcon from '~/Asset/heart-dark.json';
+import { useTheme } from '~/Theme';
 
 const LottieFav = forwardRef(({size = 40, onPress = () => {}, style = {}, progress, saved, ...props}, ref) => {
-
+  const {mode} = useTheme();
   return (
     <TouchableOpacity
       style={[{width: size, height: size}, style]}
       onPress={onPress}>
       <LottieView
         ref={ref}
-        source={favIcon}
+        source={mode === 'dark' ? darkFavIcon : lightFavIcon}
+        duration={800}
         progress={saved ? 1 : 0}
         loop={false}
       />
